b9e49ade8897aa7e5f87570010ea5ba20141a183
prakritish <bhattacharyaprakritish@gmail.com>
2026-08-09 08:29:27 +0530

    feat: add React and Vite setup with Tailwind CSS integration
    - Added React SVG logo to assets
    - Added Vite SVG logo to assets
    - Created index.css for global styles
    - Set up main.jsx as the entry point for the React application
    - Configured Vite with React and Tailwind CSS plugins

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="ticks">
+      <h1 class="text-3xl font-bold">Let's get started!</h1>
+    </div>
+  );
+}
+
+export default App;
